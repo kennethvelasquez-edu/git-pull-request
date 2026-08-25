5be99cc3ee7db4d41ba2284ec7a271e87d429fd9
Angel Contreras <acontreras-2023546@kinal.edu.gt>
2026-08-24 21:10:52 -0600

    feat: resolver problema 12 nombre mes Se arreglo la fusion en el metodo main

--- src/com/kennethvelasquez/system/Main.java
+++ src/com/kennethvelasquez/system/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
         estructuraBaseEjercicio23();
 
-        estructuraBaseEjercicio9();-
+        estructuraBaseEjercicio9();
+        estructuraBaseEjercicio12();
 
     }
 
@@ -209,7 +210,61 @@ public class Main {
      * un mes a partir de su número."
      */
     public static void estructuraBaseEjercicio12() {
-
+        int numeroMes = 9;
+        
+        switch (numeroMes){
+            case 1:
+                System.out.println("Enero");
+                break;
+                
+            case 2:
+                System.out.println("Febrero");
+                break;
+                
+            case 3:
+                System.out.println("Marzo");
+                break;
+                
+            case 4:
+                System.out.println("Abril");
+                break;
+                
+            case 5:
+                System.out.println("Mayo");
+                break;
+                
+            case 6: 
+                System.out.println("Junio");
+                break;
+                
+            case 7:
+                System.out.println("Julio");
+                break;
+                
+            case 8:
+                System.out.println("Agosto");
+                break;
+                
+            case 9:
+                System.out.println("Septiembre");
+                break;
+                
+            case 10:
+                System.out.println("Octubre");
+                break;
+                
+            case 11:
+                System.out.println("Noviembre");
+                break;
+                
+            case 12:
+                System.out.println("Diciembre");
+                break;
+                
+                default:
+                    System.out.println("El numero que indio no corresponde a un mes del año");
+                break;
+        }
     }
 
     /**
